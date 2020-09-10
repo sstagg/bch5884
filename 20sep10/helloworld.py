8e95e710cd21a799043509a5ecf17fc5e82ec39d
sstagg <sstagg@mac.com>
2020-09-10 13:23:02 -0400

    today's code

---
+++ 20sep10/helloworld.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+print("Hello World!")
+print(2**100)
+
---
+++ 20sep10/plot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy
+from matplotlib import pyplot
+
+x=numpy.arange(100)
+y=x**2
+pyplot.plot(x,y)
+pyplot.show()
+
