d89fbd137409da21a6c5ce45a74d59226a43796d
sstagg <sstagg@mac.com>
2020-09-15 11:02:19 -0400

    today

---
+++ 20sep15/floatingpoint.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+x=0.0
+while x != 10:
+	x=x+0.1
+	print (x)
+
+
+
---
+++ 20sep15/input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# This is a comment
+
+x=int(input("Please enter an integer: "))
+#x=3
+print (x)
+y=x*3
+print (y)
+
