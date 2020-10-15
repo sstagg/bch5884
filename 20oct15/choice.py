45cfdb4f630351326b81b9d9b6f184fbc45afd3c
sstagg <sstagg@mac.com>
2020-10-15 10:56:57 -0400

    Update choice.py

--- 20oct15/choice.py
+++ 20oct15/choice.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 
-choice=input("Do you want to center by mass or geomestry 'M' or 'G')
+choice=input("Do you want to center by mass or geometry 'M' or 'G': " )
 
 if choice=="M":
 	#center by mass
+	pass
 elif choice=="G":
 	#center by geometry
+	pass
